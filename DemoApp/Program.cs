ff0ff199ed1acd7f36a877ce51f313108255e966
jpmikkers <jean.paul.mikkers@gmail.com>
2024-03-28 13:15:21 +0100

    Add avalonia-blazor file picker interop demo

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -20,6 +20,7 @@ internal class Program
         appBuilder.Services.AddWindowsFormsBlazorWebView();
         appBuilder.Services.AddBlazorWebViewDeveloperTools();
         appBuilder.Services.AddSingleton<WeatherForecastService>();
+        appBuilder.Services.AddSingleton<AvaloniaFilePickerService>();
         using var myApp = appBuilder.Build();
 
         myApp.Start();
